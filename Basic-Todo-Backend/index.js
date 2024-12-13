01fa6463d0cd3152e6a1c30ea93587675ee5061b
Gaurav Kapur <gauravkapur596@gmail.com>
2024-12-13 17:28:22 +0530

    Pushing Backend of Basic Todo on the repo

---
+++ Basic-Todo-Backend/index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const {UserModel, TodoModel} = require("./db");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "Gaurav123";
+mongoose.connect("mongodb+srv://Cluster0:gaurav23@cluster0.kobs3.mongodb.net/todos-app-databse");
+app.post("/signup", async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
+
+
+    await UserModel.create({
+        email:email,
+        password: password,
+        name: name
+    })
+
+    res.json({
+        message: "You are logged in"
+    })
+});
+
+app.post("/signin", async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({
+        email: email,
+        password: password
+    })
+
+    if(user){
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, JWT_SECRET);
+        res.json({
+            token: token
+        });
+    } else {
+        res.status(403).json({
+            message: "Incorrect Credentials"
+        })
+    }
+
+});
+
+app.post("/todo",auth ,function(req,res){
+    const userId = req.userId;
+    const title = req.body.title;
+    TodoModel.create({
+        title,
+        userId
+    })
+    res.json({
+        userId: userId
+    })
+});
+
+app.post("/todos",auth ,async function(req,res){
+    const userId = req.userId;
+    const todos = await TodoModel.find({
+        userId: userId
+    })
+    res.json({
+        todos
+    })
+
+
+});
+
+function auth(req,res,next) {
+    const token = req.headers.token;
+
+    const decodeData = jwt.verify(token , JWT_SECRET);
+
+    if(decodeData) {
+        req.userId = decodeData.id;
+        next();
+    } else {
+        res.status(403).json({
+            message: "Incorrect credentials"
+        })
+    }
+}
+
+app.listen(3000);
